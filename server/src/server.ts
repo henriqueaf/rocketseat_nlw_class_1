7191a7020520502c02bd13ecb87f2ddb82873470
Henrique Almeida <henrique_af1@msn.com>
2020-06-04 01:54:08 -0300

    Add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors);
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(express.json());
